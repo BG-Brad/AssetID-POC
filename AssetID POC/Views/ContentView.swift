5238b384ee6d6bc711bd1d2d9ed3d1f83789e837
Brad Scott <brad@blackglove.com>
2021-05-25 15:44:28 -0700

    Added BG Logo, Background, Buttons

--- AssetID POC/Views/ContentView.swift
+++ AssetID POC/Views/ContentView.swift
@@ -14,10 +14,65 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
-            Text(data)
-            nfcButton(data: self.$data).frame(width: 100, height: 100)
+        ZStack {
+            Image("multi")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Image("Logo_trans")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        // .padding()
+                        .aspectRatio(contentMode: .fit)
+                    HStack {
+                        Text("Black")
+                            .font(Font.custom("BigCityGrotesquePro-Regular", size: 18))
+                            .foregroundColor(.black)
+                        Text("Glove")
+                            .font(Font.custom("BigCityGrotesquePro-Bold", size: 18))
+                            .foregroundColor(.black)
+                            .offset(x: -8)
+                    }
+                }.padding()
+                .padding(.top, 30)
+                //  Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Image(systemName: "wave.3.forward.circle")
+                            .resizable()
+                            .frame(width: 70, height: 55)
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.black)
+                            .opacity(0.8)
+                            .overlay(nfcButton(data: self.$data).frame(width: 50, height: 50) )
+                        
+                        Text("Tap To Scan NFC Tag")
+                            .font(Font.custom("BigCityGrotesquePro-Regular", size: 18))
+                            .foregroundColor(.black)
+                    }
+                    Spacer()
+                    VStack {
+                        Image(systemName: "qrcode.viewfinder")
+                            .resizable()
+                            .frame(width: 55, height: 55)
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.black)
+                            .opacity(0.8)
+                        //  .overlay(nfcButton(data: self.$data).frame(width: 50, height: 50) )
+                        Text("Tap Scan QR Code")
+                            .font(Font.custom("BigCityGrotesquePro-Regular", size: 18))
+                            .foregroundColor(.black)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
+        
     }
 }
 
