db0bd51a922892ff7c6a0bd8e33d7550845f2dea
Brad Scott <brad@blackglove.com>
2021-05-25 14:27:12 -0700

    Init Commit

---
+++ AssetID POC/App/AssetID_POCApp.swift
@@ -0,0 +1,19 @@
+//
+//  AssetID_POCApp.swift
+//  AssetID POC
+//  Created by Brad Scott on 5/25/21.
+// Property and Copyright of Black Glove Inc.
+//
+
+
+import SwiftUI
+
+@main
+struct AssetID_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
---
+++ AssetID POC/Views/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  AssetID POC
+//
+//  Created by Brad Scott on 5/25/21.
+// Property and Copyright of Black Glove Inc.
+//
+
+import SwiftUI
+import CoreNFC
+
+struct ContentView: View {
+    @State var data = ""
+    
+    
+    var body: some View {
+        VStack {
+            Text(data)
+            nfcButton(data: self.$data).frame(width: 100, height: 100)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
